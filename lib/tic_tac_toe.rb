67c51297e42f516636241163eb2a486bb986d2a1
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-03 18:04:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,3 +73,15 @@ end
 def current_player(board)
   turn_count(board).even? ? 'X' : 'O'
 end
+
+def turn(board)
+  puts 'Please enter 1-9:'
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
